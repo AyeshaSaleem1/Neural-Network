6298b5b28051a32396586f7ef44a2e9f537f9940
Ayeshasaleem1 <ayeshasaleem3598@gmail.com>
2020-03-31 14:31:15 +0500

    Neural Network Implementation-Basic

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.graph = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataSet = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.graph.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.dataSet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.graph);
+            this.tabControl1.Controls.Add(this.dataSet);
+            this.tabControl1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(-1, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1052, 516);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // graph
+            // 
+            this.graph.BackColor = System.Drawing.Color.Transparent;
+            this.graph.Controls.Add(this.chart1);
+            this.graph.Location = new System.Drawing.Point(4, 26);
+            this.graph.Name = "graph";
+            this.graph.Padding = new System.Windows.Forms.Padding(3);
+            this.graph.Size = new System.Drawing.Size(1044, 486);
+            this.graph.TabIndex = 0;
+            this.graph.Text = "Graph       ";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            this.chart1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineWidth = 3;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.White;
+            this.chart1.BorderSkin.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.HorizontalCenter;
+            this.chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Raised;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1038, 490);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // dataSet
+            // 
+            this.dataSet.Controls.Add(this.dataGridView1);
+            this.dataSet.Location = new System.Drawing.Point(4, 26);
+            this.dataSet.Name = "dataSet";
+            this.dataSet.Padding = new System.Windows.Forms.Padding(3);
+            this.dataSet.Size = new System.Drawing.Size(1044, 452);
+            this.dataSet.TabIndex = 1;
+            this.dataSet.Text = "DataSet          ";
+            this.dataSet.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(237, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(572, 496);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 20.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(393, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Neural Network";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 563);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.graph.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.dataSet.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage graph;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabPage dataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronXL;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            neural_network();
+        }
+
+        double[] input = new double[6];
+        double[] ini_weight = new double[8]; ///{0.2,0.4,-0.5,0.3,0.1,0.2,-0.3,-0.2};
+        double[] ini_bias = new double[3];// {-0.4,0.2,0.1};
+        double[] _weight = new double[8];
+        double[] _bias = new double[3];
+        double[] output = new double[6];
+        
+        int c = 0;
+        double target_output, threshold = 0.0002/* 0.001 for more perfection*/, c_error, learning_rate = 0.5, t_threshold = 0.1;
+        private void neural_network()
+        {
+            WorkBook workbook = WorkBook.Load("HW.csv");
+            WorkSheet sheet = workbook.WorkSheets.First();
+            DataTable dataTable = sheet.ToDataTable(true);
+            Random r = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                ini_weight[i] = r.NextDouble() * (0.5 - (-0.5)) + (-0.5);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                ini_bias[i] = r.NextDouble() * (1 - (-1)) + (-1);
+            }
+            while (true)//epoch
+            {
+                int j = 2;
+                double[] upd_weight = new double[8];
+                double[] upd_bias = new double[3];
+              
+                foreach (DataRow row in dataTable.Rows)
+                {
+                   // MessageBox.Show(row[0]+"");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        input[i] = Convert.ToDouble(row[i]);
+                    }
+                    target_output = Convert.ToDouble(row[3]);
+                    feed_foward(ini_weight, ini_bias);
+                    sheet["E"+j].Value = output[5];
+                    // for c_error
+                    c_error += Math.Abs( output_error);
+                        j++;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        upd_weight[i] += _weight[i];
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        upd_bias[i] += _bias[i];
+                    }
+                }
+                //average biases and weight after completing one epoch
+                for (int i = 0; i < 8; i++)
+                {
+                    upd_weight[i] = upd_weight[i]/dataTable.Rows.Count;
+                }
+                for (int i = 0; i < 3; i++)
+                {
+
+                    upd_bias[i] = upd_bias[i]/dataTable.Rows.Count;
+                }
+                c_error = c_error / dataTable.Rows.Count;
+
+                if(c_error > t_threshold)
+                {
+                    ini_weight = upd_weight;
+                    ini_bias = upd_bias;
+                    c++;
+                }
+                else 
+                {
+                   // MessageBox.Show(c_error + " " + c);
+                    workbook.SaveAs(@"C:\Users\Pc Mart\Desktop\NewExcelFile.xlsx");
+                    WorkBook wb = WorkBook.Load(@"C:\Users\Pc Mart\Desktop\NewExcelFile.xlsx");
+                    WorkSheet _sheet = wb.WorkSheets.First();
+                    _sheet.CreateChart(IronXL.Drawing.Charts.ChartType.Scatter, 2, "D", 10, "E");
+                    dataTable = _sheet.ToDataTable(true);
+                    dataGridView1.DataSource = dataTable;
+                    j = 2;
+
+                    var chart = chart1.ChartAreas[0];
+                    chart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+                    chart.AxisX.LabelStyle.Format = "";
+                    chart.AxisY.LabelStyle.Format = "";
+                    chart.AxisX.LabelStyle.IsEndLabelVisible = true;
+                    chart.AxisY.Minimum = 0.4;
+                    chart.AxisY.Interval = 0.1;
+                    chart.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+                    chart1.Series[0].IsVisibleInLegend = false;
+                    chart1.Series.Add("Target Output");
+                    chart1.Series["Target Output"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+                    chart1.Series["Target Output"].Color = Color.SeaGreen;
+
+                    chart.BackColor = Color.Black;
+                    chart1.Series.Add("Calculated Output");
+                    chart1.Series["Calculated Output"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+                    chart1.Series["Calculated Output"].Color = Color.Red;
+
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+
+                        chart1.Series["Target Output"].Points.AddY(sheet["D" + j].Value);
+                        chart1.Series["Calculated Output"].Points.AddY(sheet["E" + j].Value);
+                        j++;
+                    }
+
+                   
+                    break;
+                }
+            }
+         
+        }
+
+            double output_error = 0;
+        public void feed_foward(double[] updated_weight, double[] updated_bias )
+        {
+            output_error = 0;
+            int count = 0, wcount = 0, k = 3, bcount = 0;
+            double[] hidden_error = new double[2];
+            for (int i = 0; i < 3; i++)
+            {
+                output[i] = input[i];
+            }
+            //for hidden layers
+            for (int i = 0; i < 2; i++)
+            {
+                input[k] = input[0] * updated_weight[wcount++] + input[1] * updated_weight[wcount++] + input[2] * updated_weight[wcount++] + updated_bias[bcount++];
+                output[k] = 1 / (1 + Math.Pow( Math.E ,(-input[k])));
+                k++;
+            }
+            //for output layer
+            input[k] = output[3] * updated_weight[wcount++] + output[4] * updated_weight[wcount] + updated_bias[bcount];
+            output[k] = 1 / (1 + Math.Pow(Math.E, (-input[k])));
+
+
+            //error at output layer
+            output_error = output[k] * (1 - output[k]) * (target_output - output[k]);
+
+            if (Math.Abs( output_error) > threshold)
+            {
+                //hidden layer error
+                for (int i = 0; i < 2; i++)
+                {
+                    k--;
+                    hidden_error[i] = output[k] * (1 - output[k]) * (output_error * updated_weight[wcount--]);
+                }
+                backward_propogation(hidden_error, output_error,updated_weight,updated_bias);
+            }
+            else
+            {
+            _weight = updated_weight;
+            _bias = updated_bias;
+            }
+        }
+
+        private void backward_propogation(double[] hidden_error, double output_error,double[] up_weight,double[] up_bias)
+        {
+            double[] updated_weight = new double[8];
+            double[] updated_bias = new double[3];
+            int err_count = 1, bcount = 1, wcount = 0;
+            ///update weight
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    updated_weight[wcount] = learning_rate * hidden_error[err_count] * output[j];
+                    updated_weight[wcount] = up_weight[wcount] + updated_weight[wcount];
+                    wcount++;
+                }
+                err_count--;
+            }
+
+            for (int i = 3; i < 5; i++)
+            {
+                updated_weight[wcount] = learning_rate * output_error * output[i];
+                updated_weight[wcount] = up_weight[wcount] + updated_weight[wcount];
+                wcount++;
+            }
+
+            //update biases
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 2)
+                {
+                    updated_bias[i] = learning_rate *output_error;
+                }
+                else
+                {
+                    updated_bias[i] = learning_rate * hidden_error[bcount--];
+                }
+                updated_bias[i] = up_bias[i] + updated_bias[i];
+            }
+
+            feed_foward(updated_weight, updated_bias);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
